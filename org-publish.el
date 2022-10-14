608b8501d78f83315f8c0aed8af47a4df5889ef6
Harvey R <84394475+hnvy@users.noreply.github.com>
2022-10-15 00:30:16 +0100

    Add files via upload

---
+++ org-publish.el
@@ -0,0 +1,46 @@
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+;; I am grateful for System Crafters for posting this config online.
+;; You can find the original code at: https://systemcrafters.net/publishing-websites-with-org-mode/building-the-site/
+
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+;; Load the publishing system
+(require 'ox-publish)
+
+;; Customize the HTML output
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-export-with-sub-superscripts nil
+      org-html-head "<link rel=\"stylesheet\" href=\"https://hnvy.github.io/css/main.css\" />")
+
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "hnvy"
+             :recursive t
+             :base-directory "./src"
+             :publishing-function 'org-html-publish-to-html
+             :publishing-directory "./public"
+             :with-author nil
+             :with-toc nil
+             :section-numbers nil
+             :time-stamp-file nil)))
+
+(setq org-publish-timestamp-directory "/home/runner/work/hnvy.github.io/hnvy.github.io/public/timestamps/")
+
+;; Generate the site output
+(org-publish "hnvy" t)
---
+++ script.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+emacs -Q --script org-publish.el
